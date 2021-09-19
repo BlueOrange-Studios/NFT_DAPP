ff2b050bee363e80b6bbce8ef9d7240b81ab07e9
Youngchae James Chee <litcoderr@gmail.com>
2021-09-19 19:20:39 +0900

    main: implemented claim

--- client/index.tsx
+++ client/index.tsx
@@ -1,21 +1,57 @@
-import React from "react";
+import React, { FormHTMLAttributes } from "react";
 import ReactDOM from "react-dom";
 import engine from "./nft/api";
 
-// TODO test engine
-let e = engine;
+// test engine
+const e = engine;
 
 import "./style.css";
 
-type MainProps = {
-}
-
-function MainComponent(props: MainProps) {
-    return (
+function FormComponent() {
+    return(
         <div>
-            Main Component
+            <form id="claimForm">
+                <label> Longitude </label>
+                <input type="number" name="long"></input><br></br>
+
+                <label> Latitude </label>
+                <input type="number" name="lat"></input><br></br>
+
+                <label> Depth </label>
+                <input type="number" name="depth"></input><br></br>
+
+                <label> Name </label>
+                <input type="text" name="nft_name"></input><br></br>
+
+                <label> Kind </label>
+                <input type="text" name="kind"></input><br></br>
+
+                <button type="button" onClick={()=>{
+                    // TODO finish calim form
+                    const form = document.getElementById("claimForm") as HTMLFormElement;
+                    const long = form.elements["long"].value;
+                    const lat = form.elements["lat"].value;
+                    const depth = form.elements["depth"].value;
+                    const name = form.elements["nft_name"].value;
+                    const kind = form.elements["kind"].value;
+                    e.claim(long, lat, depth, name, kind);
+                }
+                }>claim</button>
+            </form>
         </div>
     )
 }
+
+function MainComponent() {
+    return (
+        <div>
+            <div>
+                Claim your NFT
+            </div>
+            <FormComponent></FormComponent>
+        </div>
+    )
+}
+
 
 ReactDOM.render(<MainComponent></MainComponent>, document.getElementById('root'));
--- client/nft/api.tsx
+++ client/nft/api.tsx
@@ -2,7 +2,6 @@ import Web3 from 'web3';
 import {AbiItem} from 'web3-utils';
 import {Contract} from 'web3-eth-contract';
 
-// TODO meta.address and Marmal json file should always be updated when running a new ganache server
 import meta from './meta.json';
 import token from './Marmal.json';
 import logger from '../util/logger';
@@ -29,7 +28,9 @@ class Engine {
             logger.info("initializing web3");
             if (window.ethereum) {
                 this.web3 = new Web3(window.ethereum);
-                await window.ethereum.enable();
+                try {
+                    await window.ethereum.enable();
+                }catch(e){}
             }else if (window.web3) {
                 this.web3 = new Web3(window.web3.currentProvider);
             }else {
@@ -46,13 +47,38 @@ class Engine {
         logger.info("connecting to block-chain");
 
         // init account
-        const accounts = await this.web3.eth.getAccounts();
-        this.account = accounts[0];
-        logger.info(`connected to account: ${this.account}`);
+        try {
+            const accounts = await this.web3.eth.getAccounts();
+            this.account = accounts[0];
+            logger.info(`connected to account: ${this.account}`);
+        }catch(e) {
+            logger.warn(e);
+        }
 
         // init contract
         this.contract = new this.web3.eth.Contract(token.abi as AbiItem[], meta.address);
         logger.info(`connected to contract: ${meta.address}`);
+    }
+
+    // implement
+    async claim(long: number, lat: number, depth: number, name: String, kind: String) {
+        if(this.contract && this.account) {
+            this.contract.methods.claim(
+                long,
+                lat,
+                depth,
+                name,
+                kind
+            ).send({
+                from: this.account,
+                value: Web3.utils.toWei('0.015', 'ether')
+            }).then(()=>{
+                logger.info(`(long: ${long}, lat: ${lat}, depth: ${depth}, name: ${name}, kind: ${kind}) claimed!`);
+            });
+        } else {
+            logger.error("contract or account is not initialized");
+            await this.connect();
+        }
     }
 }
 
